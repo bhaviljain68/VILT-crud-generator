f59a93e4b3b68dfa978e250082584d95e7848d67
Bhavil Jain <bhaviljain68@gmail.com>
2025-05-19 12:01:09 +0530

    Created Vue UI components

---
+++ vue-components/ui/input/DateInput.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+</script>
+
+<template>
+  <Input
+    v-bind="$attrs"
+    type="date"
+  />
+</template>
+
---
+++ vue-components/ui/input/NumberInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+</script>
+
+
+<template>
+  <Input
+    v-bind="$attrs"
+    type="number"
+  />
+</template>
+
